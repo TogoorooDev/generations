c69ed458bce99daf96daed3408d7a7b2110daf24
Yujiri <yujiri@disroot.org>
2022-01-13 17:50:33 +0000

    extract function draw_rooms

--- src/main.rs
+++ src/main.rs
@@ -9,40 +9,29 @@ fn goto(x: u16, y: u16) { println!("{}", cursor::Goto(x, y)); }
 }*/
 
 fn main() {
-    //println!("Size is {:?}", terminal_size().unwrap());
+    let mut rooms = vec!["Pretty People".to_string(), "Crypto Chat".to_string(), "Free Software Extremists".to_string(), "General".to_string()];
 
-    
-    let mut rooms: Vec<String> = vec!["Pretty People".to_string(), "Crypto Chat".to_string(), "Free Software Extremists".to_string(), "General".to_string()];
-    
-    loop {
-	let size = termion::terminal_size().unwrap();
-	let width = size.0;
-	let height = size.1;
-	let sep: u16 = width / 3;
+    let (width, height) = termion::terminal_size().unwrap();
+    let sep: u16 = width / 3;
+    clear();
+    draw_rooms(height, sep, &rooms);
+}
 
-	let mut moving_pos = (1, 1);
-	
-	clear();
-
-	// build sidebar
-
-	for y in 1..height {
-	    println!("{}|", cursor::Goto(sep, y));
-	}
-
-
-	
-	for room in rooms {
-	    println!("{}|{} ", cursor::Goto(moving_pos.0, moving_pos.1), room);
-	    moving_pos.1 += 1;
-	    for x in 2..sep {
-		println!("{}-", cursor::Goto(x, moving_pos.1));
-	    }
-	    println!("{}|", cursor::Goto(moving_pos.0, moving_pos.1));
-
-	    moving_pos.1 += 1;
-	}
-	
-	break;
+fn draw_rooms(height: u16, sep: u16, rooms: &[String]) {
+    // draw separator bar
+    for y in 1..height {
+        println!("{}|", cursor::Goto(sep, y));
+    }
+    let mut moving_pos = (1, 1);
+    for room in rooms {
+        // draw room name
+        println!("{}|{} ", cursor::Goto(moving_pos.0, moving_pos.1), room);
+        // go down and draw a separator line before the next room
+        moving_pos.1 += 1;
+        for x in 2..sep {
+            println!("{}-", cursor::Goto(x, moving_pos.1));
+        }
+        println!("{}|", cursor::Goto(moving_pos.0, moving_pos.1));
+        moving_pos.1 += 1;
     }
 }
