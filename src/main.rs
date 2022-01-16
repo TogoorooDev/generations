7b2e1a44e98dea6fc57059eef73cb39342073248
Togooroo <hens25252@protonmail.com>
2022-01-16 01:01:20 -0800

    trying again

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let mut _stdout = stdout().into_raw_mode().unwrap();
     let mut rooms = vec!["Pretty People".to_string(), "Crypto Chat".to_string(), "Free Software Extremists".to_string(), "General".to_string()];
 
-    let (mut width, mut gheight) = termion::terminal_size().unwrap();
+    let (mut width, mut height) = termion::terminal_size().unwrap();
     prep(width, height, &rooms);
     
     loop{
@@ -51,31 +51,31 @@ fn quit_menu(){
 }
 
 fn draw_rooms(height: u16, sep: u16, rooms: &[String]) {
-    // draw separator bar
-    for y in 1..height-1 {
-        print!("{}|", cursor::Goto(sep, y));
+        // draw separator bar
+        for y in 1..height-1 {
+            print!("{}|", cursor::Goto(sep, y));
     }
-    let mut y = 1;
-    for room in rooms {
-        // draw room name
-        print!("{}|{}", cursor::Goto(1, y), room);
-        // go down and draw a separator line before the next room
-        y += 1;
-        print!("{}|{}", cursor::Goto(1, y), "-".repeat((sep - 2) as usize));
-        y += 1;
+        let mut y = 1;
+        for room in rooms {
+            // draw room name
+            print!("{}|{}", cursor::Goto(1, y), room);
+            // go down and draw a separator line before the next room
+            y += 1;
+            print!("{}|{}", cursor::Goto(1, y), "-".repeat((sep - 2) as usize));
+            y += 1;
     }
 }
 
 fn draw_bottom(height: u16, width: u16) {
-    print!("{}{}", cursor::Goto(1, height-1), "-".repeat(width as usize));
+        print!("{}{}", cursor::Goto(1, height-1), "-".repeat(width as usize));
 }
 
 fn draw_messages(height: u16, width: u16, sep: u16, messages: Vec<String>) {
-	// we don't have automatic GUI-like scrolling, therefore we start from the end
-	let mut y = height - 2;
-	for message in messages.iter().rev() {
-		// let lines = message / (width - sep) + 1;
-		print!("{}{}", cursor::Goto(sep+1, y), message);
-		y -= 1;
+	    // we don't have automatic GUI-like scrolling, therefore we start from the end
+	    let mut y = height - 2;
+	    for message in messages.iter().rev() {
+		    // let lines = message / (width - sep) + 1;
+		    print!("{}{}", cursor::Goto(sep+1, y), message);
+		    y -= 1;
 	}
 }
