102a46990e8dff66cd0488e88ede70cdb2288acc
Yujiri <yujiri@disroot.org>
2022-01-16 08:01:22 +0000

    implement message wrapping

--- src/main.rs
+++ src/main.rs
@@ -15,31 +15,30 @@ fn main() {
 	
 	loop{
 
-	let (nwidth, nheight) = termion::terminal_size().unwrap();
-	if (nwidth, nheight) != (width, height){
-		width = nwidth;
-		height = nheight;
-		prep(width, height, &rooms);
-	}
-	
-	for event in stdin().keys() {
-		
-		match event.unwrap(){
-		Key::Esc => {
-			quit_menu();
-		},
-		_ => {}
-		}	    
-	}
+		let (nwidth, nheight) = termion::terminal_size().unwrap();
+		if (nwidth, nheight) != (width, height){
+			width = nwidth;
+			height = nheight;
+			prep(width, height, &rooms);
+		}
+
+		for event in stdin().keys() {
+			match event.unwrap() {
+			Key::Esc => {
+				quit_menu();
+			},
+			_ => {}
+			}
+		}
 	}
 }
 
 fn prep(width: u16, height: u16, rooms: &[String]){
 	let sep: u16 = width / 3;
 	clear();
-	draw_rooms(height, sep, &rooms);
+	draw_rooms(height, sep, rooms);
 	draw_bottom(height, width);
-	draw_messages(height, width, sep, vec!["hi".into(), "hi2".into()]);
+	draw_messages(height, width, sep, vec!["hi esr nesrot iesrt ertyu sertyu sert yuaeirt eyrt ysert yiueayr iauyer iuayer iuaewr iuwaer ieritseirt aiwetr uae rtayetr yate rywtaeryae ryutersyus eyrituy aer auer tayewrt ayewrt".into(), "hi2".into(), "hi esr nesrot iesrt ertyu sertyu sert yuaeirt eyrt ysert yiueayr iauyer iuayer iuaewr iuwaer ieritseirt aiwetr uae rtayetr yate rywtaeryae ryutersyus eyrituy aer auer tayewrt ayewrt".into()]);
 	print!("{}", cursor::Goto(1, height));
 	stdout().flush().unwrap();
 }
@@ -51,31 +50,42 @@ fn quit_menu(){
 }
 
 fn draw_rooms(height: u16, sep: u16, rooms: &[String]) {
-	        // draw separator bar
-	        for y in 1..height-1 {
-	            print!("{}|", cursor::Goto(sep, y));
+	// draw separator bar
+	for y in 1..height-1 {
+		print!("{}|", cursor::Goto(sep, y));
 	}
-	        let mut y = 1;
-	        for room in rooms {
-	            // draw room name
-	            print!("{}|{}", cursor::Goto(1, y), room);
-	            // go down and draw a separator line before the next room
-	            y += 1;
-	            print!("{}|{}", cursor::Goto(1, y), "-".repeat((sep - 2) as usize));
-	            y += 1;
+	let mut y = 1;
+	for room in rooms {
+		// draw room name
+		print!("{}|{}", cursor::Goto(1, y), room);
+		// go down and draw a separator line before the next room
+		y += 1;
+		print!("{}|{}", cursor::Goto(1, y), "-".repeat((sep - 2) as usize));
+		y += 1;
 	}
 }
 
 fn draw_bottom(height: u16, width: u16) {
-	        print!("{}{}", cursor::Goto(1, height-1), "-".repeat(width as usize));
+	print!("{}{}", cursor::Goto(1, height-1), "-".repeat(width as usize));
 }
 
 fn draw_messages(height: u16, width: u16, sep: u16, messages: Vec<String>) {
-		    // we don't have automatic GUI-like scrolling, therefore we start from the end
-		    let mut y = height - 2;
-		    for message in messages.iter().rev() {
-			    // let lines = message / (width - sep) + 1;
-			    print!("{}{}", cursor::Goto(sep+1, y), message);
-			    y -= 1;
+	let message_width = width - sep;
+	// we don't have automatic GUI-like scrolling, therefore we start from the end
+	let mut y = height - 2;
+	for message in messages.iter().rev() {
+		let chars: Vec<char> = message.chars().collect();
+		let lines = chars.len() as u16 / message_width + 1;
+		// Go to the beginning of where the message will span.
+		y -= lines - 1;
+		goto(sep+1, y);
+		let mut index: usize = 0;
+		for i in 0..lines {
+			let end = std::cmp::min(index+message_width as usize, chars.len());
+			let slice = chars[index..end].iter().collect::<String>();
+			print!("{}{}", cursor::Goto(sep+1, y+i), slice);
+			index += message_width as usize;
+		}
+		y -= 1;
 	}
 }
