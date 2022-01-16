ae40c44777e31a0567aad3f50bc17b5911155579
Yujiri <yujiri@disroot.org>
2022-01-16 06:40:10 +0000

    add rudimentary message drawing

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,6 @@ fn clear() { println!("{}{}", clear::All, cursor::Goto(1, 1)); }
 
 fn goto(x: u16, y: u16) { println!("{}", cursor::Goto(x, y)); }
 
-/*fn disp_entry(entry: string, size: ){
-    println!(
-}*/
-
 fn main() {
     let mut _stdout = stdout().into_raw_mode().unwrap();
     let mut rooms = vec!["Pretty People".to_string(), "Crypto Chat".to_string(), "Free Software Extremists".to_string(), "General".to_string()];
@@ -18,7 +14,8 @@ fn main() {
     clear();
     draw_rooms(height, sep, &rooms);
     draw_bottom(height, width);
-    print!("{}", cursor::Goto(1, height-1));
+    draw_messages(height, width, sep, vec!["hi".into(), "hi2".into()]);
+    print!("{}", cursor::Goto(1, height));
     stdout().flush().unwrap();
     for event in stdin().events() {
         break
@@ -42,5 +39,15 @@ fn draw_rooms(height: u16, sep: u16, rooms: &[String]) {
 }
 
 fn draw_bottom(height: u16, width: u16) {
-    print!("{}{}", cursor::Goto(1, height-2), "-".repeat(width as usize));
+    print!("{}{}", cursor::Goto(1, height-1), "-".repeat(width as usize));
+}
+
+fn draw_messages(height: u16, width: u16, sep: u16, messages: Vec<String>) {
+	// we don't have automatic GUI-like scrolling, therefore we start from the end
+	let mut y = height - 2;
+	for message in messages.iter().rev() {
+		// let lines = message / (width - sep) + 1;
+		print!("{}{}", cursor::Goto(sep+1, y), message);
+		y -= 1;
+	}
 }
